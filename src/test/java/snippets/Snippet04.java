5dbe221837c38cc451625eac5e8ac95d55b959f8
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-01-20 15:48:25 -0200

    feature: cancellation & evaluation

--- src/test/java/snippets/Snippet04.java
+++ src/test/java/snippets/Snippet04.java
@@ -1,4 +1,62 @@
 package snippets;
 
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@DisplayName("Misc")
 public class Snippet04 {
+
+
+    @Test
+    @DisplayName("Cancelling")
+    void f() throws InterruptedException {
+
+        Flux<Integer> integerFlux = Flux.range(1, 10).delayElements(Duration.ofMillis(250));
+
+        Disposable disposable = integerFlux
+                .doOnCancel(() -> System.out.println("Cancelled"))
+                .subscribe(e -> System.out.println("Element: "+ e));
+
+        Thread.sleep(1000);
+
+        disposable.dispose();
+
+        Thread.sleep(1000);
+
+    }
+
+
+    @Test
+    @DisplayName("Strict evaluation")
+    void f2() {
+
+        Mono.just(1/0)
+                .doOnSubscribe(e -> System.out.println("Subscribed"))
+                .doOnError(e -> System.out.println("Exception: "+e.getMessage()))
+                .block();
+
+    }
+
+    @Test
+    @DisplayName("Lazy evaluation")
+    void f3() {
+
+        Mono.fromCallable(() -> 1/0 )
+                .doOnSubscribe(e -> System.out.println("Subscribed"))
+                .doOnError(e -> System.out.println("Exception: "+e.getMessage()))
+                .block();
+
+    }
+
+
+
+
+
+
 }
